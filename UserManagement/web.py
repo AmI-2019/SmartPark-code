a1b717960d3a2cb33b70647a98c0bb525ff49f00
Andrea <andbruno97@gmail.com>
2019-05-13 21:59:50 +0200

    First general structure of the website: created login and index. database and API not yet implemented.

--- UserManagement/web.py
+++ UserManagement/web.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request, session
 
 app = Flask(__name__)
 
-
+#Redirection
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def beginning():
+    return redirect(url_for("index"))
+
+#Main page for the profiling website
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+#Page that allows a user to access to their profile:
+#Authentication via username and password
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
 
 if __name__ == '__main__':
